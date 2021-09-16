4ae25f285a9459515aa760da1a14a71c1357d386
ombasa <ombasaherman@gmail.com>
2021-09-16 23:30:25 +0300

    is published

--- listings/admin.py
+++ listings/admin.py
@@ -7,4 +7,5 @@ class ListingAdmin(admin.ModelAdmin):
 	list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
 	list_display_links = ('id', 'title')
 	list_filter = ('realtor',)
+	list_editable = ('is_published',)
 admin.site.register(Listing, ListingAdmin)
