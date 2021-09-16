2173d5ee14df42524bc5e2df781221a7a8489347
ombasa <ombasaherman@gmail.com>
2021-09-16 23:55:58 +0300

    search

--- realtors/admin.py
+++ realtors/admin.py
@@ -6,6 +6,7 @@ from .models import Realtor
 class RealtorAdmin(admin.ModelAdmin):
 	list_display = ('id', 'name', 'email', 'hire_date')
 	list_display_links = ('id', 'name')
+	search_fields = ('name',)
 
 
 
