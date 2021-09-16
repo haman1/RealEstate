7289af201c3be61f28642aaa6b1012f678cac14c
ombasa <ombasaherman@gmail.com>
2021-09-16 23:33:04 +0300

    search by

--- listings/admin.py
+++ listings/admin.py
@@ -8,4 +8,5 @@ class ListingAdmin(admin.ModelAdmin):
 	list_display_links = ('id', 'title')
 	list_filter = ('realtor',)
 	list_editable = ('is_published',)
+	search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
 admin.site.register(Listing, ListingAdmin)
