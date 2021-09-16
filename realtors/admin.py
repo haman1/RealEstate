b49ad47601cfe1e1c8c5d2c9dc29fcd3377f23df
ombasa <ombasaherman@gmail.com>
2021-09-16 23:40:30 +0300

    display links

--- realtors/admin.py
+++ realtors/admin.py
@@ -5,6 +5,8 @@ from .models import Realtor
 
 class RealtorAdmin(admin.ModelAdmin):
 	list_display = ('id', 'name', 'email', 'hire_date')
+	list_display_links = ('id', 'name')
+
 
 
 admin.site.register(Realtor, RealtorAdmin)
