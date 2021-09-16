53e48528d12b88b0494c9c6d9cd1bd688a0b27f0
ombasa <ombasaherman@gmail.com>
2021-09-16 23:59:37 +0300

    Display,pagination & filter data

--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Listing
-# Register your models here.
+
 
 
 class ListingAdmin(admin.ModelAdmin):
@@ -10,4 +10,5 @@ class ListingAdmin(admin.ModelAdmin):
 	list_editable = ('is_published',)
 	search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
 	list_per_page = 25
+
 admin.site.register(Listing, ListingAdmin)
