c10a0c21b45451999b051f416dece12765a5b5ad
ombasa <ombasaherman@gmail.com>
2021-09-22 23:16:03 +0300

    pagination, Order

--- listings/views.py
+++ listings/views.py
@@ -7,9 +7,9 @@ from .models import Listing
 
 
 def home(request):
-	listings = Listing.objects.all()
+	listings = Listing.objects.order_by('-list_date')
 
-	paginator = Paginator(listings, 3)
+	paginator = Paginator(listings, 6)
 	page = request.GET.get('page')
 	paged_listings = paginator.get_page(page)
 
