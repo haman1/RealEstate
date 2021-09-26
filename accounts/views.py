08a4b9104e25d4e2fa1c6dec49241b12d3ab5293
ombasa <ombasaherman@gmail.com>
2021-09-26 17:47:59 +0300

    views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def register(register):
+	return render(request, 'accounts/register.html')
+
+
+def login(login):
+	return render(request, 'accounts/login.html')
+
+
+def dashboard(dashboard):
+	return render(request, 'accounts/dashboard.html')
+
